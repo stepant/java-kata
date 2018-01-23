ac785777445c77286ecf7c0b8eb8ff7175775d2c
Stepan <stepan@Stepans-MacBook-Pro.local>
2018-01-24 00:34:39 +0200

    Fixed mvn test to run with JUnit5

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# java-kata
+java-kata
+FizzBuzz kata (easy)
+
+For a natural number greater than zero return:
+
+-"fizz" if the number is dividable by 3 
+-"buzz" if the number is dividable by 5 
+-"fizzbuzz" if the number is dividable by 15 
+-return the same number if no other requrement is fullfilled 
+
+Note:
+Remember to use JUnit 5 (org.junit.jupiter), 'mvn test' should be then work.
+Also you probably will need to configure the IDE to work with JUnit if pom doesn't help.
--- src/test/java/javakata/FizzBuzzTest.java
+++ src/test/java/javakata/FizzBuzzTest.java
@@ -1,29 +1,33 @@
 package javakata;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzBuzzTest {
 	@Test
 	public void testGetFizzBuzzWith15() {
+		System.out.println("Check FizzBuzz call with: 15");
 		assertEquals("fizzbuzz", FizzBuzz.getFizzBuzz(15));
 		assertEquals("fizzbuzz", FizzBuzz.getFizzBuzz(30));
 	}
 	
 	@Test
 	public void testGetFizzWith3() {
+		System.out.println("Check FizzBuzz call with: 3");
 		assertEquals("fizz", FizzBuzz.getFizzBuzz(3));
 		assertEquals("fizz", FizzBuzz.getFizzBuzz(6));
 	}
 	
 	@Test
 	public void testGetBuzzWith5() {
+		System.out.println("Check FizzBuzz call with: 4");
 		assertEquals("buzz", FizzBuzz.getFizzBuzz(5));
 		assertEquals("buzz", FizzBuzz.getFizzBuzz(10));
 	}
 	
 	@Test
 	public void testOtherNumbers() {
+		System.out.println("Check FizzBuzz call with other numbers.(1,2,4)");
 		assertEquals("1", FizzBuzz.getFizzBuzz(1));
 		assertEquals("2", FizzBuzz.getFizzBuzz(2));
 		assertEquals("4", FizzBuzz.getFizzBuzz(4));
